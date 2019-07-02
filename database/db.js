4515ed9a1854768f980e47023bdfbf201663094e
jonathancsr <jonathan.csr@hotmail.com>
2019-07-01 23:10:38 -0300

    inicio

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const db = {}
-const sequelize = new Sequelize('tpweb', 'root', '123456', {
-    host: 'localhost',
+const sequelize = new Sequelize('heroku_98b6ad814004cfe', 'b56dce79400138', '1b48863f', {
+    host: 'us-cdbr-iron-east-02.cleardb.net',
     dialect: 'mysql',
 })
 
